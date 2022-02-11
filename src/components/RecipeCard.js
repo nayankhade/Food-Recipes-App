035153cd283b7ef7f83195ae459ee83eb908ac9f
Nayan Khade <nayankhade2001@gmail.com>
2022-02-11 22:41:16 +0530

    FoodiesHub

---
+++ src/components/RecipeCard.js
@@ -0,0 +1,16 @@
+import CustomImage from "./CustomImage"
+
+export default function RecipeCard({recipe}){
+    return (
+        <div className="recipe-card">
+            <CustomImage imgSrc={recipe.image} pt="65%"/>
+            <div className="recipe-card-info">
+                <img className="auther-img" src={recipe.authorImg} alt=""/>
+                <p className="recipe-title">{recipe.title}</p>
+                <p className="recipe-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <a className="view-btn" href="https://www.foodrepublic.com/recipes/all-american-cheeseburger-recipe/
+                ">VIEW RECIPE</a>
+            </div>
+        </div>
+    )
+}
